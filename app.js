7368e962411e9f7c00748fa09dea45caf920f905
priyeshkh <khatiwadapriyesh@gmail.com>
2024-06-14 03:13:03 -0700

    just random static site based on my interest

---
+++ app.js
@@ -0,0 +1,7 @@
+function handleCreatePost() {
+  document.getElementById("post-form-container").style.display = "block";
+}
+
+function hideForm() {
+  document.getElementById("post-form-container").style.display = "none";
+}
